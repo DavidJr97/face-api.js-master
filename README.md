7488e2cf75dcc054e661a879c104aa0e68be09a5
DavidJr97 <36302181+DavidJr97@users.noreply.github.com>
2020-05-05 22:01:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Face-api.js
+
+Face-api.js es una libreria usada para el reconocimiento de rostos en imagenes o videos, esta libreria se implementa puede ser implementada en una aplicación web o móvil.
+
+
+
+# Teconolgias usadas
+
+1. [React js](https://es.reactjs.org/)
+2. [Face-api.js documentación 1](https://justadudewhohacks.github.io/face-api.js) - [face-api.js documentacion 2](https://github.com/justadudewhohacks/face-api.js)
+3. [React-redux](https://www.youtube.com/watch?v=HhtqSwUgP1U&t=1875s)
+4. [Programacion funcional](https://medium.com/laboratoria-developers/introducci%C3%B3n-a-la-programaci%C3%B3n-funcional-en-javascript-parte-1-e0b1d0b2142e)
+
+
+
+
+# Sitios para descargar SVG
+
+En los ejemplos de este repositorio se incluyen archivos SVG, estos han sido descargados de los sitios siguientes:
+
+1. [svgrepo](https://www.svgrepo.com/vectors/hat/)
+2. [onlinewebfonts](https://www.onlinewebfonts.com/icon/search?q=sunglas)
---
+++ src/FacePage/index.js
@@ -0,0 +1,282 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from "react-redux";
+import mapStateToProps from './mapStateToProps';
+import mapDispatchToProps from './mapDispatchToProps';
+import Camera from './Camera';
+import Canva from './Canva';
+import * as faceapi from 'face-api.js';
+import ScrollButton from './ScrollButton';
+
+class FacePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            controller: 'game',
+            loading: false,
+            authorized: false,
+            checkAutorization: true,
+            positionIndex: 0,
+            filterName: 'guate1',
+            imageFilter: new Image(),
+            showFilter: true,
+            ejeXe: 0,
+            ejeYe:20,
+        }
+        this.setVideoHandler = this.setVideoHandler.bind(this);
+        this.isModelLoaded = this.isModelLoaded.bind(this);
+    }
+
+    async setVideoHandler() {
+        if (this.isModelLoaded() !== undefined) {
+            try {
+                let result = await faceapi.detectSingleFace(this.props.video.current, this.props.detector_options).withFaceLandmarks().withFaceExpressions().withAgeAndGender();
+                if (result !== undefined) {
+                    console.log("face detected", 1);
+                    const dims = faceapi.matchDimensions(this.props.canvas.current, this.props.video.current, true);
+                    const resizedResult = faceapi.resizeResults(result, dims);
+                    faceapi.draw.drawDetections(this.props.canvas.current, resizedResult);
+                 //   faceapi.draw.drawFaceLandmarks(this.props.canvas.current, resizedResult);
+
+                    const currentCanvas = ReactDOM.findDOMNode(this.props.canvas.current);
+                    var canvasElement = currentCanvas.getContext("2d");
+                    this.addFilter(canvasElement, result);
+                   // this.addBoxIndexOfLandmark(canvasElement, result.landmarks.positions[this.state.positionIndex]);
+                    this.addBackgroundInformation(canvasElement, result);
+                    this.addGenderAndAgeInformation(canvasElement, result);
+                    this.addEmotionInformation(canvasElement, resizedResult, result);
+
+                } else {
+                    console.log("face detected", 1);
+                }
+            } catch (exception) {
+                console.log(exception);
+            }
+        }
+        setTimeout(() => this.setVideoHandler());
+    }
+
+   // addBoxIndexOfLandmark(canvasElement, landkmarkPosition) {
+     //   let width = 10, height = 10;
+      //  canvasElement.setTransform(1, 0, 0, 1, 0, 0);
+      //  canvasElement.fillStyle = 'rgb(255, 87, 51)';
+      //  canvasElement.fillRect(landkmarkPosition.x, landkmarkPosition.y, width, height);
+       // canvasElement.closePath();
+       // canvasElement.setTransform(1, 0, 0, 1, 0, 0);
+   // }
+
+    addBackgroundInformation(canvasElement, result) {
+        let positionX = result.landmarks.positions[8].x,
+            positionY = result.landmarks.positions[8].y + 10;
+        canvasElement.fillStyle = "black";
+        canvasElement.fillRect(positionX - 45, positionY - 12, 90, 45);
+    }
+
+    addGenderAndAgeInformation(canvasElement, result) {
+        // Edad y Sexo
+        canvasElement.font = "10px Comic Sans MS";
+        //canvasElement.font="30px Arial";
+        canvasElement.fillStyle = "red";
+        let positionX = result.landmarks.positions[8].x,
+            positionY = result.landmarks.positions[8].y + 10,
+            gender = (result.gender) === "male" ? "Hombre" : "Mujer",
+            age = "Edad: " + result.age.toFixed();
+        gender = "Sexo: " + gender;
+
+        canvasElement.textAlign = "center";
+        canvasElement.fillStyle = "white";
+        canvasElement.fillText(gender, positionX, positionY);
+        canvasElement.fillText(age, positionX, positionY + 15);
+    }
+
+    addEmotionInformation(canvasElement, resizedResult, result) {
+        const expressions = resizedResult.expressions;
+        const maxValue = Math.max(...Object.values(expressions));
+        let emotion = Object.keys(expressions).filter(
+            item => expressions[item] === maxValue
+        );
+        emotion = emotion[0];
+        emotion = (emotion === "happy") ? "feliz" : emotion;
+        emotion = (emotion === "neutral") ? "neutral" : emotion;
+        emotion = (emotion === "angry") ? "enojado" : emotion;
+        emotion = (emotion === "sad") ? "triste" : emotion;
+        emotion = (emotion === "surprised") ? "sorprendido" : emotion;
+        emotion = (emotion === "fearful") ? "temeroso" : emotion;
+
+        let positionX = result.landmarks.positions[8].x,
+            positionY = result.landmarks.positions[8].y + 10;
+        canvasElement.fillText("Emocion: " + emotion, positionX, positionY + 30);
+    }
+
+    addFilter(canvasElement, result) {
+        let startIndex = 0, endIndex = 16, ajustX = (this.state.ejeXe), ajustY = (this.state.ejeYe);
+        let positionX1 = result.landmarks.positions[startIndex].x - ajustX,
+            positionY1 = result.landmarks.positions[startIndex].y + ajustY,
+            positionX2 = result.landmarks.positions[endIndex].x + ajustX,
+            positionY2 = result.landmarks.positions[endIndex].y + ajustY,
+            m = ((positionY2 - positionY1) / (positionX2 - positionX1)) * 100;
+
+        let width = positionX2 - positionX1,
+            height = width * 0.8;
+
+        positionY1 -= (height / 4);
+        positionY2 -= (height / 4);
+
+        var TO_RADIANS = Math.PI / 180,
+            angleInRad = (m / 2.5) * TO_RADIANS;
+        console.log("TO_RADIANS", TO_RADIANS);
+
+        canvasElement.setTransform(1, 0, 0, 1, 0, 0);
+        canvasElement.translate(positionX1, positionY1 - 50);
+        canvasElement.rotate(angleInRad);
+        canvasElement.drawImage(this.state.imageFilter, 0, 0, width, height);
+        /*canvasElement.translate(positionX1 ,positionY1) 
+        canvasElement.translate(1,0,0,0,positionX1+(width/2),positionY1); 
+        canvasElement.rotate(angleInRad);    */
+        //canvasElement.drawImage(this.state.imageFilter,0,0,width,height);
+        //canvasElement.restore();
+        canvasElement.setTransform(1, 0, 0, 1, 0, 0);
+        //this.rotateAndPaintImage(canvasElement, this.state.imageFilter, angleInRad, positionX1, positionY1,20,0 );
+    }
+
+    rotateAndPaintImage(context, image, angleInRad, positionX, positionY, axisX, axisY) {
+        context.translate(positionX, positionY);
+        context.rotate(angleInRad);
+        context.drawImage(image, -axisX, -axisY);
+        context.rotate(-angleInRad);
+        context.translate(-positionX, -positionY);
+    }
+
+    isModelLoaded() {
+        if (this.props.selected_face_detector === this.props.SSD_MOBILENETV1) return faceapi.nets.ssdMobilenetv1.params;
+        if (this.props.selected_face_detector === this.props.TINY_FACE_DETECTOR) return faceapi.nets.tinyFaceDetector.params;
+    }
+
+
+    async componentDidMount() {
+        console.log("height: " + window.screen.height + ", width: " + window.screen.width);
+
+        // obtener parametros de configuracion y asignar el modelo que vamos a usar para reconocer rostros
+        this.setDetectorOptions();
+        this.props.SET_VIDEO_HANDLER_IN_GAME_FACENET(this.setVideoHandler);
+
+        // asignar los archivos del model a face-api
+        let modelFolder = "/models";
+
+        let dirs = { 
+         guate1: '/filter/escudo.svg',
+         guate2: '/filter/bandera.png', 
+        }
+        
+
+        let valor = 'guate1'
+        try {
+            await faceapi.loadFaceLandmarkModel(modelFolder);
+            await faceapi.nets.ageGenderNet.loadFromUri(modelFolder);
+            await faceapi.nets.faceExpressionNet.loadFromUri(modelFolder);
+            if (this.props.selected_face_detector === this.props.SSD_MOBILENETV1) await faceapi.nets.ssdMobilenetv1.loadFromUri(modelFolder);
+            if (this.props.selected_face_detector === this.props.TINY_FACE_DETECTOR) await faceapi.nets.tinyFaceDetector.load(modelFolder);
+
+            this.state.imageFilter.src = (dirs[valor]);
+            this.state.imageFilter.onload = function () {
+                console.log("image is loaded");
+
+            }
+        } catch (exception) {
+            console.log("exception", exception);
+        }
+    }
+
+
+    async componentDidUpdate() {
+        console.log('El estado ha cambiado')
+        this.props.SET_VIDEO_HANDLER_IN_GAME_FACENET(this.setVideoHandler);
+
+        // asignar los archivos del model a face-api
+        let modelFolder = "/models";
+
+        let dirs = { 
+        guate1: '/filter/escudo.svg', 
+        guate2: '/filter/bandera.png', 
+        guate3: '/filter/quetzal.png',
+        guate4: '/filter/logo.png',
+        guate5: '/filter/drinkplace.svg',
+        guate6: '/filter/ball.svg',
+        guate7: '/filter/fashion.svg',
+        guate8: '/filter/holidays.svg',
+        guate9: '/filter/nature.svg',
+        guate10: '/filter/protection.svg',
+        guate11: '/filter/sand-bucket.svg',
+        guate12: '/filter/beach.svg',
+        guate13: '/filter/sea-animal.svg',
+        guate14: '/filter/summertime.svg',
+        guate15: '/filter/swimsuit.svg',
+        
+     }
+
+
+
+        let valor = this.state.filterName
+        try {
+            await faceapi.loadFaceLandmarkModel(modelFolder);
+            await faceapi.nets.ageGenderNet.loadFromUri(modelFolder);
+            await faceapi.nets.faceExpressionNet.loadFromUri(modelFolder);
+            if (this.props.selected_face_detector === this.props.SSD_MOBILENETV1) await faceapi.nets.ssdMobilenetv1.loadFromUri(modelFolder);
+            if (this.props.selected_face_detector === this.props.TINY_FACE_DETECTOR) await faceapi.nets.tinyFaceDetector.load(modelFolder);
+
+            this.state.imageFilter.src = (dirs[valor]);
+            this.state.imageFilter.onload = function () {
+                console.log("image is loaded");
+
+            }
+        } catch (exception) {
+            console.log("exception", exception);
+        }
+
+    }
+    setDetectorOptions() {
+        let minConfidence = this.props.min_confidence,
+            inputSize = this.props.input_size,
+            scoreThreshold = this.props.score_threshold;
+
+        // identificar el modelo previsamente entrenado para reconocer rostos.
+        // el modelo por defecto es tiny_face_detector
+        let options = this.props.selected_face_detector === this.props.SSD_MOBILENETV1
+            ? new faceapi.SsdMobilenetv1Options({ minConfidence })
+            : new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold });
+        this.props.SET_DETECTOR_OPTIONS_IN_GAME_FACENET(options);
+    }
+
+  
+switchFilter(e){
+
+    this.setState({ filterName: e.target.value, ejeX: 0, ejeYe:80 })
+
+}
+    render() {
+        return (
+            <div>
+                <Camera />
+                <Canva />
+
+                <input type="number"
+                    style={{ marginLeft: 1000 }}
+                    value={this.state.positionIndex}
+                    onChange={(event) => { this.setState({ positionIndex: event.target.value }) }} />
+
+                <button type="button" value='guate1' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 0, ejeYe:20 }) }}>ESCUDO</button>
+                <button type="button" value='guate2' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 0, ejeYe:20 }) }}>BANDERA</button>
+                <button type="button" value='guate3' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 50, ejeYe:100 }) }}>QUETZAL</button>
+                <button type="button" value='guate4' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 0, ejeYe:80 }) }}>LOGO</button>
+                <button type="button" value='guate5' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 0, ejeYe:80 }) }}>LENTES 3</button>
+                <button type="button" value='guate6' onClick={(event) => { this.setState({ filterName: event.target.value, ejeX: 0, ejeYe:80 }) }}>LENTES 3</button>
+
+                <h1>{this.state.filterName}</h1>
+                <h1>{this.state.ejeX}</h1>
+                <h1>{this.state.ejey}</h1>
+                   </div>
+        )
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FacePage);
